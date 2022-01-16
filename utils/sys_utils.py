aba46ee1dcfc948c49ef3dbff1e121ac7c6c9d3d
Gonzalo Romero <gonzaromero2007@gmail.com>
2022-01-16 17:19:56 -0300

    Add functions to get memory, cpu and gpu usage

--- utils/sys_utils.py
+++ utils/sys_utils.py
@@ -1,4 +1,7 @@
 import os
+
+import nvidia_smi
+import psutil
 import torch
 
 
@@ -24,3 +27,33 @@ def suppress_tf_warnings():
 
 def suppress_sklearn_errors():
     os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+
+def get_memory_usage():
+    percentage = psutil.virtual_memory()[2]
+    print(f"RAM memory used: {percentage}%")
+    return percentage
+
+
+def get_cpu_usage():
+    cpu_usage = psutil.cpu_percent(4)
+    print(f"CPU used: {cpu_usage}%")
+    return cpu_usage
+
+
+def get_gpu_usage():
+    try:
+        nvidia_smi.nvmlInit()
+        deviceCount = nvidia_smi.nvmlDeviceGetCount()
+        gpu_usage = {}
+        for i in range(deviceCount):
+            handle = nvidia_smi.nvmlDeviceGetHandleByIndex(i)
+            info = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
+            gpu_usage[nvidia_smi.nvmlDeviceGetName(handle)] = (
+                100 * info.free / info.total
+            )
+        print(f"GPU used: {gpu_usage}")
+        return gpu_usage
+    except:
+        print("WARNING: Cannot get gpu usage")
+
