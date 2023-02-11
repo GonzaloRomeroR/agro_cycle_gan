fbf2dcaa03ee4df36d422ffe61cf5d0803686643
Gonzalo Romero Roman <gonzaromero2007@gmail.com>
2023-02-11 14:36:14 +0100

    Add docstrings to tests

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,5 +1,3 @@
-import sys
-
 from train import train, Config
 from generate import ImageTransformer
 from pathlib import Path
@@ -8,6 +6,10 @@ file_path = Path(__file__).parent.resolve()
 
 
 def test_training():
+    """
+    This test only checks that a training can be done
+    without crashing with a simple image example
+    """
     config = Config()
     config.image_resize = [64, 64]
     config.use_dataset = "testA2testB"
@@ -19,6 +21,10 @@ def test_training():
 
 
 def test_generation():
+    """
+    This test only checks that a generation can be done
+    without crashing with a simple image example
+    """
     image_transformer = ImageTransformer("testA2testB")
     image_transformer.transform_dataset(
         origin_path=f"{file_path}/../../images/testA2testB/test_A/A",
