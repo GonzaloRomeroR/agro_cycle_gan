73fc705c7b8e4cc72eafc30d2d3f0f60db875c00
Gonzalo Romero Roman <gonzaromero2007@gmail.com>
2023-02-20 20:02:57 +0100

    Upload missing trainer changes

--- trainers/basic_trainer.py
+++ trainers/basic_trainer.py
@@ -11,27 +11,17 @@ class BasicTrainer(BaseTrainer):
         self.lambda_cycle = 5
         self.lambda_identity = 10
 
-    def get_discriminator_losses(
-        self, a_real, b_real, a_fake, b_fake, old_a_fake, old_b_fake, iters, epoch
-    ):
+    def get_discriminator_losses(self, a_real, b_real, a_fake, b_fake):
 
         disc_losses = {}
         for disc in [self.D_A, self.D_B]:
             disc_name = "DISC_A" if disc == self.D_A else "DISC_B"
             im_real = a_real if disc == self.D_A else b_real
             im_fake = a_fake if disc == self.D_A else b_fake
-            old_im_fake = old_a_fake if disc == self.D_A else old_b_fake
             D_losses = self.D_A_losses if disc == self.D_A else self.D_B_losses
 
             disc.optimizer.zero_grad()
-            if (iters > 0 or epoch > 0) and iters % 3 == 0:
-                rand_int = random.randint(5, old_im_fake.shape[0] - 1)
-                disc_loss = disc.get_loss(
-                    disc(im_real),
-                    disc(old_im_fake[rand_int - 5 : rand_int].detach()),
-                )
-            else:
-                disc_loss = disc.get_loss(disc(im_real), disc(im_fake.detach()))
+            disc_loss = disc.get_loss(disc(im_real), disc(im_fake.detach()))
 
             D_losses.append(disc_loss.item())
             disc_loss.backward()
@@ -97,13 +87,9 @@ class BasicTrainer(BaseTrainer):
                     a_real, b_real, self.G_A2B, self.G_B2A
                 )
 
-                if iters == 0 and epoch == 0:
-                    old_b_fake = b_fake.clone()
-                    old_a_fake = a_fake.clone()
-
                 # Discriminator
                 disc_losses = self.get_discriminator_losses(
-                    a_real, b_real, a_fake, b_fake, old_a_fake, old_b_fake, iters, epoch
+                    a_real, b_real, a_fake, b_fake
                 )
 
                 gen_losses = self.get_generator_losses(
@@ -114,18 +100,6 @@ class BasicTrainer(BaseTrainer):
                 for losses in [gen_losses, disc_losses]:
                     for name, value in losses.items():
                         self.losses_epoch[name].append(value.item())
-
-                # Update old image fake
-                if iters == 0 and epoch == 0:
-                    old_b_fake = b_fake.clone()
-                    old_a_fake = a_fake.clone()
-                elif old_b_fake.shape[0] == self.bs * 5 and b_fake.shape[0] == self.bs:
-                    rand_int = random.randint(5, 24)
-                    old_b_fake[rand_int - 5 : rand_int] = b_fake.clone()
-                    old_a_fake[rand_int - 5 : rand_int] = a_fake.clone()
-                elif old_b_fake.shape[0] < 25:
-                    old_b_fake = torch.cat((b_fake.clone(), old_b_fake))
-                    old_a_fake = torch.cat((a_fake.clone(), old_a_fake))
 
                 iters += 1
 
