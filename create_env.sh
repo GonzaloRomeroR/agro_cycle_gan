565be3e0048c34dcae838c69853be6cab99ba1b1
Gonzalo Romero Roman <gonzaromero2007@gmail.com>
2023-05-28 19:46:06 +0200

    Add script to install packages

---
+++ create_env.sh
@@ -0,0 +1,5 @@
+# Small script to install the dependencies using venv
+
+python -m venv environments/agro_cycle_gan_env
+source environments/agro_cycle_gan_env/Scripts/activate
+pip install -r environments/requirements.txt
